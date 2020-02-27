1ec8fe57fe8ddb9249f58b2e4bcf35df324680a6
Kai Peterson <pete9372@umn.edu>
2020-02-27 14:44:56 -0600

    add dice icons and style buttons

--- src/components/DiceRoller/DiceRoller.js
+++ src/components/DiceRoller/DiceRoller.js
@@ -1,9 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import './DiceRoller.css';
 
 function DiceRoller() {
     return (
-        <div>
-            DICE ROLLER THAT RANDOMALLY ROLLS ANY COMBINATION OF DIE GOES HERE
+        <div className="dice-roller-container">
+
+            <div className="dice-container">
+                <div>
+                    <img src="/images/d4.png" />
+                    <button>D4</button>
+                </div>
+                <div>
+                    <img src="/images/d6.png" />
+                    <button>D6</button>
+                </div>
+                <div>
+                    <img src="/images/d8.png" />
+                    <button>D8</button>
+                </div>
+                <div>
+                    <img src="/images/d10.png" />
+                    <button>D10</button>
+                </div>
+                <div>
+                    <img src="/images/d12.png" />
+                    <button>D12</button>
+                </div>
+                <div>
+                    <img src="/images/d20.png" />
+                    <button>D20</button>
+                </div>
+            </div>
+
         </div>
     )
 }
